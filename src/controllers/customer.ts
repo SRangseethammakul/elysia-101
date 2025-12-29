7d883e458f0996fac7336d196588a99e69d3660f
srangseethammakul <srangseethammakul@gmail.com>
2025-12-29 16:18:49 +0700

    add dynamic path

--- src/controllers/customer.ts
+++ src/controllers/customer.ts
@@ -1,9 +1,13 @@
 import { Elysia } from "elysia";
 
-const customerController = new Elysia().get("/customers", () => {
-  return {
-    data: ["customers data"],
-  };
-});
+const customerController = new Elysia()
+  .get("/customers", () => {
+    return {
+      data: ["customers data"],
+    };
+  })
+  .get("customers/:id", ({ params: { id } }) => {
+    return { id };
+  });
 
 export default customerController;
