0718667ad53f9b2369437cfca592b895b8f8a1dd
srangseethammakul <srangseethammakul@gmail.com>
2025-12-29 21:37:26 +0700

    add  validator and  beforeHandle,afterHandle

--- src/controllers/customer.ts
+++ src/controllers/customer.ts
@@ -1,4 +1,4 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 
 const customerController = new Elysia()
   .get("/customers", () => {
@@ -6,10 +6,30 @@ const customerController = new Elysia()
       data: ["customers data"],
     };
   })
-  .get("customers/:id", ({ params: { id } }) => {
-    return { id };
-  })
-  .post("customers", ({ body }) => {
+  .get(
+    "customers/:id",
+    ({ params: { id }, status }) => {
+      console.log("processing");
+      if (id > 100) {
+        return status(400, "Bad Request");
+      }
+      return { id };
+    },
+    {
+      params: t.Object({
+        id: t.Number({ error: `id must be number` }),
+      }),
+      beforeHandle: () => {
+        console.log("before process");
+      },
+      afterHandle: () => {
+        console.log("after process");
+      },
+    }
+  )
+  .post("customers", ({ body, set }) => {
+    // set.status = "Created";
+    set.status = 201;
     return body;
   })
   .patch("customers/:id/update", ({ params: { id }, body }) => {
