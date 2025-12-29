ac04e3e56b3a72acf21f068c6e3374bd15f70e98
srangseethammakul <srangseethammakul@gmail.com>
2025-12-29 16:31:09 +0700

    add method get post patch delete

--- src/controllers/customer.ts
+++ src/controllers/customer.ts
@@ -8,6 +8,18 @@ const customerController = new Elysia()
   })
   .get("customers/:id", ({ params: { id } }) => {
     return { id };
+  })
+  .post("customers", ({ body }) => {
+    return body;
+  })
+  .patch("customers/:id/update", ({ params: { id }, body }) => {
+    return {
+      id,
+      body,
+    };
+  })
+  .delete("customers/:id", ({ params: { id } }) => {
+    return `deleted success ${id}`;
   });
 
 export default customerController;
